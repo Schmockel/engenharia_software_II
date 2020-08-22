5abfc2409d711c2f55a6b4418215eec7773da383
Schmockel <danielpschmockel@hotmail.com>
2020-08-21 22:45:08 -0300

    rodando o servidor

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("olá!")
+const express = require('express');
+const server = express();
+
+server.get('/', function(request, response){
+    response.send('<h1> ainda funcionando</h1>');
+})
+
+
+
+server.listen(3333, function(){
+  console.log("tudo funcionando");
+});
