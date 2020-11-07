f70d0056a27e82c6cb698c082799e860b0bb34c5
Schmockel <danielpschmockel@hotmail.com>
2020-11-06 22:07:40 -0200

    códigos atualizados a aula de 06/11/2020

--- src/infra/http/routes/finance.routes.js
+++ src/infra/http/routes/finance.routes.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
-const entryController = require('../../../modules/finance/controllers/entryController');
+const movimentController = require('../../../modules/finance/controllers/movimentController');
 const authenticated = require('../middlewares/authenticated');
 
 const financeRouter = Router();
 
-financeRouter.post('/entry', authenticated, entryController.create);
+financeRouter.post('/moviment', authenticated, movimentController.create);
+financeRouter.delete('/moviment/:id', authenticated, movimentController.delete);
+financeRouter.get('/moviment', authenticated, movimentController.index);
+financeRouter.get('/moviment/:id', authenticated, movimentController.show);
 
 module.exports = financeRouter;
